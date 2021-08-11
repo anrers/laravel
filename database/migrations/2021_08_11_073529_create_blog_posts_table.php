1d72311d26629bf5649d28da51ea0dc1aec305a7
Pyzhyk <pyzhykvv@gmail.com>
2021-08-11 12:19:52 +0300

    create blog and blog post model

---
+++ database/migrations/2021_08_11_073529_create_blog_posts_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+
+            $table->string('slug')->unique();
+            $table->string('title')->unique();
+
+            $table->text('excerpt')->nullable();
+
+            $table->text('content_raw');
+            $table->text('content_html');
+
+            $table->boolean('is_published')->default(false);
+            $table->timestamp('published_at')->nullable();
+
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index('is_published');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
